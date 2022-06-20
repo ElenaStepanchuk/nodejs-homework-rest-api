969d6a5ac2d71686d306ecc5469c0a9fc5d9b43e
ElenaStepanchuk <podolyak365@gmail.com>
2022-06-20 21:52:12 +0300

    add filter by favorite

--- controllers/contacts/listContacts.js
+++ controllers/contacts/listContacts.js
@@ -1,11 +1,13 @@
 const { Contact } = require("../../models");
 const listContacts = async (req, res) => {
   const { _id } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite = false } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner: _id }, "", {
+
+  const result = await Contact.find({ owner: _id, favorite }, "", {
     skip,
     limit: Number(limit),
+    favorite,
   }).populate("owner", "_id email");
   res.json({
     status: "success",
